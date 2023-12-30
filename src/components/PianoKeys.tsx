10537aeeb1c9f4961e475fa743bc524a75861524
Nick Taylor <nick@nickyt.co>
2023-12-29 19:41:19 -0500

    chore: wip for collabiano

---
+++ src/components/PianoKeys.tsx
@@ -0,0 +1,60 @@
+export type Note =
+  | "C4"
+  | "D4"
+  | "E4"
+  | "F4"
+  | "G4"
+  | "A4"
+  | "B4"
+  | "C5"
+  | "C#4"
+  | "D#4"
+  | "F#4"
+  | "G#4"
+  | "A#4";
+
+interface PianoKeyProps {
+  note: string;
+  playNote: (note: Note) => void;
+}
+
+export const WhitePianoKey = ({ note, playNote }: PianoKeyProps) => {
+  return (
+    <button
+      className="border border-black rounded-sm bg-color-white"
+      style={{ width: "3rem", height: "12rem" }}
+      data-note={note}
+      onClick={(event) => {
+        const { note } = event.currentTarget.dataset;
+
+        if (!note) {
+          throw new Error("No note found on key");
+        }
+
+        playNote(note as Note);
+      }}
+    >
+      <span className="sr-only">{`piano key ${note}`}</span>
+    </button>
+  );
+};
+
+export const BlackPianoKey = ({ note, playNote }: PianoKeyProps) => {
+  return (
+    <button
+      className="black-key"
+      data-note={note}
+      onClick={(event) => {
+        const { note } = event.currentTarget.dataset;
+
+        if (!note) {
+          throw new Error("No note found on key");
+        }
+
+        playNote(note as Note);
+      }}
+    >
+      <span className="sr-only">{`piano key ${note}`}</span>
+    </button>
+  );
+};
